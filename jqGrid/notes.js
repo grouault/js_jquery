7235a1637966e9af3a5b75b774e319c761d3f59f
grouault <gildasrouault@gmail.com>
2017-07-21 10:25:55 +0200

    Create notes.js

---
+++ jqGrid/notes.js
@@ -0,0 +1,4 @@
+// recuperation de l'identifiant de la ligne
+var myGrid = $('#list'),
+selRowId = myGrid.jqGrid ('getGridParam', 'selrow'),
+celValue = myGrid.jqGrid ('getCell', selRowId, 'columnName');
