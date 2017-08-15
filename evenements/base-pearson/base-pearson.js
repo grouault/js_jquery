0c2525bdb1ec99eb9ab150346fc112b1bb89da5f
ROUAULT Gildas <grouault@OFFICEPOMONA.FR>
2017-08-15 09:14:19 +0200

    Ajout Event

--- evenements/base-pearson/base-pearson.js
+++ evenements/base-pearson/base-pearson.js
@@ -1,3 +1,43 @@
 /**
  * Created by grouault on 14/08/2017.
  */
+$(function(){
+
+    var $body = $('body');
+    var $container = $('#container');
+    var $switcher = $('#switcher');
+
+    var $switcherDefault = $switcher.find('#switcher-default');
+    var $switcherNarrow = $switcher.find('#switcher-narrow');
+    var $switcherLarge = $switcher.find('#switcher-large');
+
+    var $buttons = $switcher.find('.button');
+
+    $switcher.find('h3').click(function () {
+        $buttons.toggle('slow');
+    });
+
+    $buttons.hover(function () {
+        $(this).addClass('hover')
+    }, function () {
+        $(this).removeClass('hover')
+    });
+
+    $buttons.click(function(e){
+        console.log('buttons : this = ', this);
+        $body.removeClass();
+        $buttons.removeClass('selected');
+        $(this).addClass('selected');
+        if ( $(this).is('#switcher-narrow') ) {
+            $body.addClass('narrow');
+        }
+        if ( $(this).is('#switcher-large') ) {
+            $body.addClass('large');
+        }
+    });
+
+    $('span').click(function(e){
+        console.log('span - this = ', this);
+    });
+
+});
--- evenements/base/base-event.js
+++ evenements/base/base-event.js
@@ -1,3 +1,28 @@
 /**
  * Created by grouault on 14/08/2017.
  */
+$(function(){
+
+
+    $('a.link').click(function(e){
+        console.log('event on a.link - e', e.target);
+    });
+
+    $('span.link').click(function(e){
+        console.log('event on span.link - e', e.target);
+    });
+
+
+    $('div.link').click(function(e){
+        console.log('event on div.link - e', e.target);
+        if ($(e.target).is('a.link')){
+            console.log('J\'ai cliqué sur le lien');
+        } else {
+            console.log('J\'ai cliqué ailleurs que sur le lien');
+        }
+    });
+
+    // simulation de click.
+    $('span.link').trigger('click');
+
+});
